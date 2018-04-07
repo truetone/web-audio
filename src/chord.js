31fc7fa5ccb5791272e380ec97d155f57c52919d
Tony Thomas <thoma127@umn.edu>
2018-04-07 15:24:41 -0500

    WIP Chord whitespace changes

--- src/chord.js
+++ src/chord.js
@@ -1,39 +1,38 @@
 const tone = require("./tone.js");
 
 class Chord {
-    constructor(ctx, toneType, gain, pan, panValue, chordNotes, notes) {
-        const self = this;
-        this.connected = false;
-        this.tones = [];
+  constructor(ctx, toneType, gain, pan, panValue, chordNotes, notes) {
+    const self = this;
+    this.connected = false;
+    this.tones = [];
 
-        pan.pan.value = panValue;
+    pan.pan.value = panValue;
 
-        chordNotes.forEach((note) => {
-            console.log("Creating new tone for note: ", note);
-            const t = new tone(ctx, toneType, gain, pan, notes);
-            t.playNote(note);
-            self.tones.push(t);
-        });
+    chordNotes.forEach((note) => {
+        console.log("Creating new tone for note: ", note);
+        const t = new tone(ctx, toneType, gain, pan, notes);
+        t.playNote(note);
+        self.tones.push(t);
+    });
+  }
 
-    }
+  connect() {
+    const self = this;
+    this.connected = true;
+    self.tones.forEach((t) => {
+        console.log("Connecting tone: ", t.name);
+        t.connect();
+    });
+  }
 
-    connect() {
-        const self = this;
-        this.connected = true;
-        self.tones.forEach((t) => {
-            console.log("Connecting tone: ", t.name);
-            t.connect();
-        });
-    }
-
-    disconnect() {
-        const self = this;
-        this.connected = false;
-        self.tones.forEach((t) => {
-            console.log("Disconnecting tone: ", t.name);
-            t.disconnect();
-        });
-    }
+  disconnect() {
+    const self = this;
+    this.connected = false;
+    self.tones.forEach((t) => {
+        console.log("Disconnecting tone: ", t.name);
+        t.disconnect();
+    });
+  }
 }
 
 module.exports = Chord;
