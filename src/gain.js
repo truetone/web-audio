359208ad954aacae2f4ed492f1d6242139f44dc7
Tony Thomas <thoma127@umn.edu>
2018-04-07 15:16:59 -0500

    WIP Gain refactor

--- src/gain.js
+++ src/gain.js
@@ -1,18 +1,17 @@
 class Gain {
   constructor(ctx, name, value) {
-    console.log("Setting gain value to ", value);
     this.name = name + ".Gain";
     this.gain = ctx.createGain();
     this.set(value);
   }
 
   set(value) {
+    console.log("Setting gain value to ", value);
     this.gain.gain.value = value;
   }
 
-  change(value) {
-    console.log("Setting gain value to ", value);
-    this.gain.gain.value = value;
+  get() {
+    return this.gain.gain.value;
   }
 }
 
