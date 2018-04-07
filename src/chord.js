5bfc9d9673eaa33397d71c8008c35f2f7be312d1
Tony Thomas <thoma127@umn.edu>
2018-04-07 15:36:25 -0500

    WIP Chord - refactored with new interface for Tone

--- src/chord.js
+++ src/chord.js
@@ -1,34 +1,31 @@
 const tone = require("./tone.js");
 
 class Chord {
-  constructor(ctx, toneType, gain, pan, panValue, chordNotes, notes) {
+  constructor(ctx, name, toneType, gainValue, panValue, chordNotes, notes) {
     const self = this;
     this.connected = false;
     this.tones = [];
-
-    pan.pan.value = panValue;
+    this.name = name + ".Chord";
 
     chordNotes.forEach((note) => {
         console.log("Creating new tone for note: ", note);
-        const t = new tone(ctx, toneType, gain, pan, notes);
+        const t = new tone(ctx, self.name, toneType, gainValue, panValue, notes);
         t.playNote(note);
         self.tones.push(t);
     });
   }
 
   connect() {
-    const self = this;
     this.connected = true;
-    self.tones.forEach((t) => {
+    this.tones.forEach((t) => {
         console.log("Connecting tone: ", t.name);
         t.connect();
     });
   }
 
   disconnect() {
-    const self = this;
     this.connected = false;
-    self.tones.forEach((t) => {
+    this.tones.forEach((t) => {
         console.log("Disconnecting tone: ", t.name);
         t.disconnect();
     });
